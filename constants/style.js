47354aba9e61953fc5e8abaf4c168ff737a1c752
Matheus Lima <matheusinclima@gmail.com>
2022-09-14 19:59:20 -0300

    including folders

---
+++ constants/style.js
@@ -0,0 +1,25 @@
+export const GlobalStyles = {
+  colors: 
+  {
+    primary50: '#f0eaff',
+    primary100: '#d1c2f4',
+    primary200: '#b09ae6',
+    primary300: '#9072da',
+    primary400: '#714acf',
+    primary500: '#5730b5',
+    primary600: '#44268e',
+    primary700: '#301a67',
+    primary800: '#1d0f3f',
+    primary900: '#0c031b',
+    accent50: '#fafcdf',
+    accent100: '#eff4b9',
+    accent200: '#e4ec90',
+    accent300: '#d9e465',
+    accent400: '#cedd3c', //intensity lemmom
+    accent500: '#b4c322',
+    accent600: '#8c9818',
+    accent700: '#646c0e',
+    accent800: '#3b4104',
+    accent900: '#141700',  
+  },
+};
